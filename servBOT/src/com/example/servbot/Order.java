13ad0936be78eb919e2e7303535c1ecafa53c222
Jromer94 <Jromer94@gmail.com>
2013-11-15 19:45:01 -0500

    app is ready to recieve menu json from server

--- servBOT/src/com/example/servbot/Order.java
+++ servBOT/src/com/example/servbot/Order.java
@@ -14,44 +14,38 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
-public class RestaurantList extends Activity {
-	public final static String EXTRA_MESSAGE = "com.example.servbot.MESSAGE";
+public class Order extends Activity {
+	String name;
+	String table;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		StrictMode.ThreadPolicy policy = new StrictMode.
-			    ThreadPolicy.Builder().permitAll().build();
-		StrictMode.setThreadPolicy(policy);
-		setContentView(R.layout.activity_restaurant_list);
-		
-		final ListView listview = (ListView) findViewById(R.id.listview);
-		 
-		String readRestaurantList = readRestaurantList();
+		setContentView(R.layout.activity_order);
+		name = getIntent().getStringExtra(RestaurantMenu.TEST1);
+		table = getIntent().getStringExtra(RestaurantMenu.TEST2);
 		String values[] = new String[1];
-		    try {
-		      JSONArray jsonArray = new JSONArray(readRestaurantList);
-		      values = new String[jsonArray.length()];
-		      for (int i = 0; i < jsonArray.length(); i++) {
-		        values[i] = jsonArray.getString(i);
-		      }
-		    } catch (Exception e) {
-		      e.printStackTrace();
-		    }
-		  
-
+		setTitle(name);
+		String getMenu = getMenu(name);
+	    try {
+	      JSONArray jsonArray = new JSONArray(getMenu);
+	      values = new String[jsonArray.length()];
+	      for (int i = 0; i < jsonArray.length(); i++) {
+	        JSONObject jsonObject = jsonArray.getJSONObject(i);
+	        values[i] = jsonObject.getString("food") + " : $" + jsonObject.getString("price"); 
+	      }
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+	    final ListView listview = (ListView) findViewById(R.id.listview);
+	   
 	    final ArrayList<String> list = new ArrayList<String>();
 	    for (int i = 0; i < values.length; ++i) {
 	      list.add(values[i]);
@@ -59,29 +53,21 @@ public class RestaurantList extends Activity {
 	    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 	        android.R.layout.simple_list_item_1, list);
 	    listview.setAdapter(adapter);
-	    listview.setOnItemClickListener(new OnItemClickListener() {
-
-	        @Override
-	        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-	                long arg3) {
-	            restaurantMenu(arg1  );
-
-	        }
-
-	    });
-		
-}
+	    
+	
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.restaurant_list, menu);
+		getMenuInflater().inflate(R.menu.order, menu);
 		return true;
 	}
-	public String readRestaurantList() {
+
+	public String getMenu(String name) {
 	    StringBuilder builder = new StringBuilder();
 	    HttpClient client = new DefaultHttpClient();
-	    HttpGet httpGet = new HttpGet("http://servbot.herokuapp.com/restaurants");
+	    HttpGet httpGet = new HttpGet("http://servbot.herokuapp.com/menu/" + name);
 	    try {
 	      HttpResponse response = client.execute(httpGet);
 	      StatusLine statusLine = response.getStatusLine();
@@ -104,14 +90,5 @@ public class RestaurantList extends Activity {
 	    }
 	    return builder.toString();
 	  }
-	
-	public void restaurantMenu(View view){
-		
-		Intent intent = new Intent(this, RestaurantMenu.class);
-		String textOfSelectedItem  =((TextView) view).getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, textOfSelectedItem);
-		startActivity(intent);
-		
-	}
+} 
 
-}
--- servBOT/src/com/example/servbot/RestaurantMenu.java
+++ servBOT/src/com/example/servbot/RestaurantMenu.java
@@ -1,15 +1,30 @@
 package com.example.servbot;
 
-import android.os.Bundle;
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class RestaurantMenu extends Activity {
-
+	public final static String TEST1 = "com.example.servbot.MESSAGE1";
+	public final static String TEST2 = "com.example.servbot.MESSAGE2";
+	String name;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_restaurant_menu);
+		name = getIntent().getStringExtra(RestaurantList.EXTRA_MESSAGE);
+;		setContentView(R.layout.activity_restaurant_menu);
+		setTitle(name);
 	}
 
 	@Override
@@ -18,5 +33,29 @@ public class RestaurantMenu extends Activity {
 		getMenuInflater().inflate(R.menu.restaurant_menu, menu);
 		return true;
 	}
+	
+	public void sendTable(View view){
+		EditText editText = (EditText) findViewById(R.id.test);
+		String table = editText.getText().toString();
+		HttpClient httpclient = new DefaultHttpClient();
+	    HttpPost httppost = new HttpPost("http://servbot.herokuapp.com/restaurants/" + name + "/" + table);
+	    try {
+			HttpResponse response = httpclient.execute(httppost);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    Intent intent = new Intent(this, Order.class);
+	    intent.putExtra(TEST1, name);
+	    intent.putExtra(TEST2, table);
+	    startActivity(intent);
+		
+	}
 
 }
+
+
+
